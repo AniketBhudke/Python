f633d0040709f811fb872bf3c48b2df9e19fea34
AniketBhudke <bhudkea@gmail.com>
2024-12-17 10:47:44 +0530

    problem solving

--- gg/p.py
+++ gg/p.py
@@ -22,5 +22,29 @@
 #                 return arr.index(x)
 #         else:
 #             return -1
-        
-        
+
+
+# Given two arrays: a[] and b[], where both arrays may contain duplicate elements. The task is to determine whether array b is a subset of array a. It's important to note that both arrays can be unsorted. Additionally, each occurrence of a duplicate element within an array is considered as a separate element of the set.
+
+# Examples:
+
+# Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+# Output: Yes
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [1, 2, 3, 4, 4, 5, 6], b[] = [1, 2, 4]
+# Output: Yes
+# Explanation: b[] is a subset of a[]
+# Input: a[] = [10, 5, 2, 23, 19], b[] = [19, 5, 3]
+# Output: No
+# Explanation: b[] is not a subset of a[]
+# a=[12,3,4,5,6,7,8,9]
+# b=[3,4,5,6]
+       
+# a=set(a)
+# b=set(b)
+# # Your code here
+# res=b.issubset(a)
+# print(res)
+
+a="aniket"
+print(a[::-1])
