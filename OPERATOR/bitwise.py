84e9a9b3632229a160eb54c58057204aa87ca9d1
AniketBhudke <bhudkea@gmail.com>
2025-03-16 23:49:02 +0530

    practice question

---
+++ OPERATOR/bitwise.py
@@ -0,0 +1,20 @@
+#bitwise AND
+# a=5
+# b=99
+# print(a&b)#------------odd number asla tr 1 output dete
+# number = 7
+# print( number & 1)
+# if number & 1:  # If LSB is 1, number is odd
+#     print(f"{number} is odd")
+# else:
+#     print(f"{number} is even")
+
+#bitwise OR
+# a=5
+# b=3
+# print(a|b)
+
+#bitwise XOR
+a=0
+b=1
+print(a^b)
---
+++ OPERATOR/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+number = 7
+# print( number & 1)
+# if number & 1:  # If LSB is 1, number is odd
+#     print(f"{number} is odd")
+# else:
+#     print(f"{number} is even")
