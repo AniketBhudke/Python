c2bc5c1e3e5f1b8b84a40916ef2502151fe3d73d
AniketBhudke <bhudkea@gmail.com>
2025-04-05 01:31:23 +0530

    2

--- django/prog.py
+++ django/prog.py
@@ -1,4 +1,25 @@
 #command line argument 
 
-import sys
-print(sys.argv)
+# import sys
+# print(sys.argv)
+
+
+#some example
+t=(10,15,20)
+x,_,y=t
+print(x,_,y) #output: 10 20
+
+a=1_2
+print(a*2)
+
+x,y=4,3
+print((x--x)+(y--y))
+
+print(pow(2,7,5))
+
+
+n=6
+while n>0:
+    print(n)
+    n-=2 if n%3==0 else 1
+    
