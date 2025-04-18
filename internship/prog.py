1b2b9c09ad922712dd2c19cbeb7642d4da3f6c87
AniketBhudke <bhudkea@gmail.com>
2025-04-18 08:59:36 +0530

    1st day

--- internship/prog.py
+++ internship/prog.py
@@ -28,3 +28,33 @@ lst=[1, 2, 4, 5, 6]
 # print(duplicatenumber(lst,lst1))
 
 
+
+#find dupliacte number
+# Input: [3, 1, 3, 4, 2]
+# Output: 3
+
+# def duplicatenumber(lst,lst1):
+#     for i in range(len(lst)):
+#         if lst.count(lst[i])>=2:
+#             lst1.append(lst[i])
+#     return set(lst1)
+# lst1=[]
+# lst=[3, 1, 3, 4, 2]
+# print(duplicatenumber(lst,lst1))
+
+
+
+#find dupliacte number
+# Input: [3, 1, 3, 4, 2]
+# Output: 3
+
+# def duplicatenumber(lst,lst1):
+#     for i in range(len(lst)):
+#         if lst.count(lst[i])>=2:
+#             lst1.append(lst[i])
+#     return set(lst1)
+# lst1=[]
+# lst=[3, 1, 3, 4, 2]
+# print(duplicatenumber(lst,lst1))
+
+
