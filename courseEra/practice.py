c780991b9fce41046f784d837dbacd4d0b1cc3d9
AniketBhudke <bhudkea@gmail.com>
2025-08-26 23:07:19 +0530

    Updated code

--- courseEra/practice.py
+++ courseEra/practice.py
@@ -1,11 +1,17 @@
-def fizzbuzz(number): 
-    if number%3==0 and number%5==0:
-        return "FizzBuzz"   
-    elif number%3==0:
-        return "Fizz"
-    elif number%5==0:
-        return "Buzz"
-    else:
-        return number
-p=fizzbuzz(15)      
-print(p)
+# def fizzbuzz(number): 
+#     if number%3==0 and number%5==0:
+#         return "FizzBuzz"   
+#     elif number%3==0:
+#         return "Fizz"
+#     elif number%5==0:
+#         return "Buzz"
+#     else:
+#         return number
+# p=fizzbuzz(15)      
+# print(p)
+
+# x = 1 + 2 * 3 - 8 / 4
+# print(x)
+
+x=(1,2,3,4,5)
+print(max(x))
