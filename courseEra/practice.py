185a5e58309ce679d15d217c159e0dad09c7f315
AniketBhudke <bhudkea@gmail.com>
2025-08-29 21:07:03 +0530

    Updated code

--- courseEra/practice.py
+++ courseEra/practice.py
@@ -13,5 +13,33 @@
 # x = 1 + 2 * 3 - 8 / 4
 # print(x)
 
-x=(1,2,3,4,5)
-print(max(x))
+# x=(1,2,3,4,5)
+# print("Hello World")
+
+# print(max(x))
+
+# def stuff():
+#     print('Hello')
+#     return
+#     print('World')
+
+# # stuff()
+# tot = 0 
+# for i in [5, 4, 3, 2, 1] :
+#     tot = tot + 1
+#     print(tot)
+# print(tot)\\
+
+zork = 0
+for thing in [9, 41, 12, 3, 74, 15] :
+    zork = zork + thing
+print('After', zork)
+
+arr=(12,3,4,5)
+x=8
+for val in arr:
+    if len(arr)>2:
+        if val == x:
+            print(arr.index(val))
+    else:
+        print(0)
