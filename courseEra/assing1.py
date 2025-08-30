a8497b4fa1f9d27b4b723e6e27da4d2e5f1dd546
AniketBhudke <bhudkea@gmail.com>
2025-08-31 01:06:05 +0530

    clg assignment question

--- courseEra/assing1.py
+++ courseEra/assing1.py
@@ -1,19 +1,75 @@
-# number1=int("Enter The First Number:")
-# number2=int("Enter The Second Number:")
-# print("Sum OF two Number:",number1+number2)
+#sum of two number
+num1=int(input("Enter 1st Number:"))
+num2=int(input("Enter 2nd Number:"))
+print("Sum Of Two Number:",num1+num2)
 
-# n=int(input("Enter The First Number:"))
-# sum=0
-# if n>0:
-#     while n>0:
-#         r=n%10
-#         sum=sum+r
-#         n=n//10
-# print(sum)    
+print(70*"-")
+num1=int(input("Enter 1st Number:"))
+num2=int(input("Enter 2nd Number:"))
+if num1>num2:
+    print(num1,"is Greater")
+else:
+    print(num2,"ids greater")
+
+num1=int(input("Enter 1st Number:"))
+num2=int(input("Enter 2nd Number:"))
+if num1>num2:
+    print(num1,"is Greater")
+elif num2>num1:
+    print(num2,"is greater")
+else:
+    print("Both Are Equal")
+print(70*"-")
+
+    
+num1=int(input("Enter 1st Number:"))
+num2=int(input("Enter 2nd Number:"))
+num3=int(input("Enter 3rd Number:"))
+if num1>num2 and num1>num3:
+    print(num1,"is Greater")
+elif num2>num1 and num2>num3:
+    print(num2,"is greater")
+elif num3>num1 and num3>num2:
+    print(num3,"Is Greater")
+else:
+    print("three are equal")
+print(70*"-")
+
+num1=int(input("Enter 1st Number:"))
+for i in range(1,num1+1):
+    print(i)
+print(70*"-")
 
 
-st=True
-t1=(1,2,3,4,1,5)
-for i in range(len(t1)):
-    if t1.count(t1[i])>2:
-        
+num1=int(input("Enter 1st Number:"))
+for i in range(1,num1+1):
+    if i%2==0:
+        print(i)
+print(70*"-")
+
+
+num1=int(input("Enter 1st Number:"))
+for i in range(1,num1+1):
+    if i%2!=0:
+        print(i)
+print(70*"-")
+
+num1=int(input("Enter 1st Number:"))
+s=0
+for i in range(1,num1+1):
+    s=s+i
+print(s)
+print(70*"-")
+
+print(70*"-")
+num1=int(input("Enter 1st Number:"))
+for i in range(1,num1+1):
+    if num1%i==0:
+        print(i)
+print(70*"-")
+
+num1=int(input("Enter 1st Number:"))
+for i in range(num1,-num1,-1):
+    print(i)
+    
+print(70*"-")
